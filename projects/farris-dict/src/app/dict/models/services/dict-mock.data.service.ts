135258d688e0c90a96f618d7768fdb13f5e722ff
xuke <kkhsu123@163.com>
2020-07-06 15:38:46 +0800

    CRUD动作

--- projects/farris-dict/src/app/dict/models/services/dict-mock.data.service.ts
+++ projects/farris-dict/src/app/dict/models/services/dict-mock.data.service.ts
@@ -32,9 +32,6 @@ export class DictMockDataService {
     result.returnValue = data;
     return of(result);
   }
-  private fromRawData(data: any): DictData {
-    return JSON.parse(JSON.stringify(data)) as DictData;
-  }
 
   /**
    * 查询数据
@@ -61,5 +58,59 @@ export class DictMockDataService {
       total: DictMockData.length,
     };
     return of(result);
+  }
+
+  /*
+   * 获取新建数据默认值
+   */
+  getDefaultValue() {
+    const result = new ResponseInfo();
+    result.code = "0";
+    const item = {
+      id: "ID_" + new Date().valueOf().toString(),
+      name: "",
+      code: "",
+    };
+    result.returnValue = [item];
+    return of(result);
+  }
+  /**
+   * 保存数据
+   * @param data 所要保存的数据
+   */
+  save(data: DictData): Observable<ResponseInfo> {
+    const result = new ResponseInfo();
+    const index = DictMockData.findIndex((item) => item.id === data.id);
+    if (index !== -1) {
+      // 修改
+      DictMockData[index] = data;
+      result.code = "0";
+    } else {
+      // 新增
+      DictMockData.push(data);
+      result.code = "0";
+    }
+    result.returnValue = [data];
+    return of(result);
+  }
+  /**
+   * 删除数据
+   * @param id 所要删除数据的标识
+   */
+  remove(id: string): Observable<ResponseInfo> {
+    const index = DictMockData.findIndex((item) => item.id === id);
+    const result = new ResponseInfo();
+    if (index !== -1) {
+      DictMockData.splice(index, 1);
+      result.code = "0";
+    } else {
+      result.message = "无效的Id";
+      result.code = "1";
+    }
+    return of(result);
+  }
+
+  private fromRawData(data: any): DictData {
+    return JSON.parse(JSON.stringify(data)) as DictData;
   }
 }
